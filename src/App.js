a288443f177ccb25f25f9bc0085f2001ebb85cd4
AnushkaaSharma <sanushkaa4@gmail.com>
2021-05-16 22:08:01 +0545

    rendered folder

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+    <div>
+      <Folder name='Desktop'>
+        <File name= 'dogs.jpg' />
+      </Folder>
+      <Folder name='Applications' />
     </div>
-  );
+  )
+  
+}
+const Folder = (props) => {
+  return <div style={{ border: '2px solid black' }}>
+    { props.name }
+    <div>{ props.children }</div>
+  </div>
+    
+}
+
+const File = (props) => {
+  return <div>{ props.name }</div>
 }
 
 export default App;
